6cf803609cf9fc986ca8dc8c72ca77d738c2ac58
DamianGaleano9 <dj.damian.b@hotmail.com>
2025-01-31 16:42:11 +0100

    Create Rev-Rotate and Roatate with his subFunctions move_ra, move_rb_ move_rr

--- moves/rev_rotate.c
+++ moves/rev_rotate.c
@@ -6,7 +6,45 @@
 /*   By: dmazo-ga <dmazo-ga@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/31 12:09:14 by dmazo-ga          #+#    #+#             */
-/*   Updated: 2025/01/31 12:09:15 by dmazo-ga         ###   ########.fr       */
+/*   Updated: 2025/01/31 16:24:59 by dmazo-ga         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include	"../push_swap.h"
+
+static void	rev_rotate(t_stack **stack)
+{
+	t_stack	*tmp;
+	t_stack	*tmp2;
+	t_stack	*tmp3;
+
+	tmp = *stack;
+	tmp2 = *stack;
+	while (tmp2->next)
+	{
+		tmp3 = tmp2;
+		tmp2 = tmp2->next;
+	}
+	*stack = tmp2;
+	tmp2->next = tmp;
+	tmp3->next = NULL;
+}
+
+void	move_rra(t_stack **a)
+{
+		rev_rotate(a);
+		ft_printf("rra\n");
+}
+
+void	move_rrb(t_stack **b)
+{
+	rev_rotate(b);
+	ft_printf("rrb\n");
+}
+
+void	move_rrr(t_stack **a, t_stack **b)
+{
+	rev_rotate(a);
+	rev_rotate(b);
+	ft_printf("rrr\n");
+}
--- moves/rotate.c
+++ moves/rotate.c
@@ -6,7 +6,41 @@
 /*   By: dmazo-ga <dmazo-ga@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/31 12:09:08 by dmazo-ga          #+#    #+#             */
-/*   Updated: 2025/01/31 12:09:09 by dmazo-ga         ###   ########.fr       */
+/*   Updated: 2025/01/31 16:41:35 by dmazo-ga         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "../push_swap.h"
+
+static void	rotate(t_stack **stack)
+{
+		t_stack	*tmp;
+		t_stack	*tmp2;
+
+		tmp = *stack;
+		*stack = (*stack)->next;
+		tmp2 = *stack;
+		while (tmp2->next)
+			tmp2 = tmp2->next;
+		tmp2->next = tmp;
+		tmp->next = NULL;
+}
+
+void	move_ra(t_stack **a)
+{
+		rotate(a);
+		ft_printf("ra\n");
+}
+
+void	move_rb(t_stack **b)
+{
+		rotate(b);
+		ft_printf("rb\n");
+}
+
+void	move_rr(t_stack **a, t_stack **b)
+{
+		rotate(a);
+		rotate(b);
+		ft_printf("rr\n");
+}
